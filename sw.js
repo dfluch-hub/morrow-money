2de2ef1e315de42ea06b0e06cbcc44e18b89f72e
dfluch-hub <d.fluch@gmx.at>
2026-09-17 14:44:03 +0200

    Bump PWA cache for final UX polish

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'morrow-money-core-v17';
-const DYNAMIC_CACHE = 'morrow-money-dynamic-v17';
+const CACHE_NAME = 'morrow-money-core-v18';
+const DYNAMIC_CACHE = 'morrow-money-dynamic-v18';
 
 const LOCAL_ASSETS = [
   './',
