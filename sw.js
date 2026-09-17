655b80fd548530fb7a98d509ff32f6f1906020a0
dfluch-hub <d.fluch@gmx.at>
2026-09-17 22:11:54 +0200

    Bump Morrow Money PWA cache to v26

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'morrow-money-core-v25';
-const DYNAMIC_CACHE = 'morrow-money-dynamic-v25';
+const CACHE_NAME = 'morrow-money-core-v26';
+const DYNAMIC_CACHE = 'morrow-money-dynamic-v26';
 const LOCAL_ASSETS = ['./','./index.html','./manifest.json','./icon-192x192.png','./icon-512x512.png'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(LOCAL_ASSETS)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(names=>Promise.all(names.filter(name=>name!==CACHE_NAME&&name!==DYNAMIC_CACHE).map(name=>caches.delete(name)))).then(()=>self.clients.claim()))});
