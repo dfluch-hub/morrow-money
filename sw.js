02964cf852e8af342952e1a4d9ae367019cf2a10
dfluch-hub <d.fluch@gmx.at>
2026-09-17 19:36:43 +0200

    Bump Morrow Money cache to v22

--- sw.js
+++ sw.js
@@ -1,57 +1,6 @@
-const CACHE_NAME = 'morrow-money-core-v21';
-const DYNAMIC_CACHE = 'morrow-money-dynamic-v21';
-
-const LOCAL_ASSETS = [
-  './',
-  './index.html',
-  './manifest.json',
-  './icon-192x192.png',
-  './icon-512x512.png'
-];
-
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => cache.addAll(LOCAL_ASSETS))
-      .then(() => self.skipWaiting())
-  );
-});
-
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((cacheNames) => Promise.all(
-      cacheNames
-        .filter((name) => name !== CACHE_NAME && name !== DYNAMIC_CACHE)
-        .map((name) => caches.delete(name))
-    )).then(() => self.clients.claim())
-  );
-});
-
-self.addEventListener('fetch', (event) => {
-  if (event.request.method !== 'GET') return;
-
-  if (event.request.mode === 'navigate') {
-    event.respondWith(
-      fetch(event.request)
-        .then((response) => {
-          const copy = response.clone();
-          caches.open(CACHE_NAME).then((cache) => cache.put('./index.html', copy));
-          return response;
-        })
-        .catch(() => caches.match('./index.html'))
-    );
-    return;
-  }
-
-  event.respondWith(
-    caches.match(event.request).then((cachedResponse) => {
-      if (cachedResponse) return cachedResponse;
-      return fetch(event.request).then((networkResponse) => {
-        if (!networkResponse || networkResponse.status !== 200 || (networkResponse.type !== 'basic' && networkResponse.type !== 'cors')) return networkResponse;
-        const responseToCache = networkResponse.clone();
-        caches.open(DYNAMIC_CACHE).then((cache) => cache.put(event.request, responseToCache));
-        return networkResponse;
-      });
-    })
-  );
-});
+const CACHE_NAME = 'morrow-money-core-v22';
+const DYNAMIC_CACHE = 'morrow-money-dynamic-v22';
+const LOCAL_ASSETS = ['./','./index.html','./manifest.json','./icon-192x192.png','./icon-512x512.png'];
+self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(LOCAL_ASSETS)).then(()=>self.skipWaiting()))});
+self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(names=>Promise.all(names.filter(name=>name!==CACHE_NAME&&name!==DYNAMIC_CACHE).map(name=>caches.delete(name)))).then(()=>self.clients.claim()))});
+self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;if(event.request.mode==='navigate'){event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE_NAME).then(cache=>cache.put('./index.html',copy));return response}).catch(()=>caches.match('./index.html')));return}event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request).then(response=>{if(!response||response.status!==200||(response.type!=='basic'&&response.type!=='cors'))return response;const copy=response.clone();caches.open(DYNAMIC_CACHE).then(cache=>cache.put(event.request,copy));return response}))) });
