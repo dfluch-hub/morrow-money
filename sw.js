063eebed08507a940fa47b7d59f591f927363ed7
dfluch-hub <d.fluch@gmx.at>
2026-09-18 21:01:32 +0200

    Bump PWA cache to v54 after CTA repair

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'morrow-money-core-v53';
-const RUNTIME_CACHE = 'morrow-money-runtime-v53';
+const CACHE_NAME = 'morrow-money-core-v54';
+const RUNTIME_CACHE = 'morrow-money-runtime-v54';
 const APP_SHELL = [
   './',
   './index.html',
