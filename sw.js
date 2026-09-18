81bd8ee8ad90947cb71509fafb47777f1f658ded
dfluch-hub <d.fluch@gmx.at>
2026-09-18 20:31:06 +0200

    Bump PWA cache to v50 for coaching completion

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'morrow-money-core-v49';
-const RUNTIME_CACHE = 'morrow-money-runtime-v49';
+const CACHE_NAME = 'morrow-money-core-v50';
+const RUNTIME_CACHE = 'morrow-money-runtime-v50';
 const APP_SHELL = [
   './',
   './index.html',
