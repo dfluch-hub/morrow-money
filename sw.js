60bbf69af59d7b99148e01c75978630742482fbb
dfluch-hub <d.fluch@gmx.at>
2026-09-18 18:55:51 +0200

    Bump PWA cache for welcome screen redesign

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'morrow-money-core-v44';
-const RUNTIME_CACHE = 'morrow-money-runtime-v44';
+const CACHE_NAME = 'morrow-money-core-v45';
+const RUNTIME_CACHE = 'morrow-money-runtime-v45';
 const APP_SHELL = [
   './',
   './index.html',
