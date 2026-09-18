d90bead70234a2ad8189bc7d9d41636708dda092
dfluch-hub <d.fluch@gmx.at>
2026-09-18 17:16:19 +0200

    Bump PWA cache after language and copy fixes

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'morrow-money-core-v42';
-const RUNTIME_CACHE = 'morrow-money-runtime-v42';
+const CACHE_NAME = 'morrow-money-core-v43';
+const RUNTIME_CACHE = 'morrow-money-runtime-v43';
 const APP_SHELL = [
   './',
   './index.html',
