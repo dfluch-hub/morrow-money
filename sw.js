9e4c9bc0d32ec81e71bc583ca1043f82aaf0929f
dfluch-hub <d.fluch@gmx.at>
2026-09-18 09:23:01 +0200

    Bump PWA cache for alignment refinements

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'morrow-money-core-v33';
-const DYNAMIC_CACHE = 'morrow-money-dynamic-v33';
+const CACHE_NAME = 'morrow-money-core-v34';
+const DYNAMIC_CACHE = 'morrow-money-dynamic-v34';
 const LOCAL_ASSETS = ['./','./index.html','./manifest.webmanifest','./icon-192x192.png','./icon-512x512.png'];
 
 self.addEventListener('install', event => {
